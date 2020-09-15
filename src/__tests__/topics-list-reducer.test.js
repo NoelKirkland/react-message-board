1692283d78190ee33cf6af5d8c2e17279048b210
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-15 10:31:37 -0700

    'add add_topic case to topics list reducer

--- src/__tests__/topics-list-reducer.test.js
+++ src/__tests__/topics-list-reducer.test.js
@@ -3,5 +3,22 @@ import topicsListReducer from './../reducers/topics-list-reducer';
 describe('topicsListReducer', ()=>{
   test('should return the default state if no action is passed to the reducer', ()=>{
     expect(topicsListReducer({}, {action: null})).toEqual({});
-  })
-})
+  });
+
+  test('should add a topic to the topics list', ()=>{
+    const action = {
+      type: "ADD_TOPIC",
+      name: 'Politics',
+      threads: {},
+      id: 1
+    };
+
+    expect(topicsListReducer({}, action)).toEqual({
+      1: {
+        name: 'Politics',
+        threads: {},
+        id: 1
+      }
+    });
+  });
+});
--- src/reducers/topics-list-reducer.js
+++ src/reducers/topics-list-reducer.js
@@ -1,3 +1,15 @@
 export default (state = {}, action) => {
-  return state;
+  const {name, threads, id} = action;
+  switch (action.type) {
+    case 'ADD_TOPIC':
+      return Object.assign({}, state, {
+        [id]: {
+          name: name,
+          threads: threads,
+          id: id
+        }
+      });
+      default:
+        return state;
+  }
 };
