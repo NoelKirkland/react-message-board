40a70d95cee540e2f51f802d0f8820dfb3192693
Micheal Hansen <micheal.hansen@gmail.com>
2020-09-16 10:39:41 -0700

    add actions and tests for rest of actions
    Co-authored-by: Ben Russell <benjaminrussell36@gmail.com>

--- src/__tests__/actions.test.js
+++ src/__tests__/actions.test.js
@@ -17,4 +17,75 @@ describe('actions', ()=>{
       id: 1
     });
   });
+
+  it('should create addThread action', ()=>{
+    const newThread = {name: 'testThread', topicId: 1, id: 1};
+    expect(a.addThread(newThread)).toEqual({
+      type: c.ADD_THREAD,
+      name: 'testThread',
+      topicId: 1,
+      id: 1
+    });
+  });
+
+  it('should create deleteThread action', ()=>{
+    expect(a.deleteThread(1)).toEqual({
+      type: c.DELETE_THREAD,
+      id: 1
+    });
+  });
+
+  it('should create addPost action', ()=>{
+    const time = new Date().toString();
+    const newPost = {
+      title: "This should be in the first politics post",
+      body: "That's where it should be, for sure",
+      date: time,
+      score: 0,
+      id: 'post1',
+      threadId: 'thread1',
+      topicId: 'topic1'
+    }
+    expect(a.addPost(newPost)).toEqual({
+      type: c.ADD_POST,
+      title: "This should be in the first politics post",
+      body: "That's where it should be, for sure",
+      date: time,
+      score: 0,
+      id: 'post1',
+      threadId: 'thread1',
+      topicId: 'topic1'
+    });
+  });
+
+  it('should create deletePost action', ()=>{
+    expect(a.deletePost(1)).toEqual({
+      type: c.DELETE_POST,
+      id: 1
+    });
+  });
+
+  it('viewIndex should create VIEW_INDEX action', ()=>{
+    expect(a.viewIndex()).toEqual({
+      type: c.VIEW_INDEX,
+      topicId: null,
+      threadId: null
+    });
+  });
+
+  it('viewTopic should create a VIEW_TOPIC action', ()=>{
+    expect(a.viewTopic(1)).toEqual({
+      type: c.VIEW_TOPIC,
+      topicId: 1,
+      threadId: null
+    });
+  });
+
+  it('viewThread should create a VIEW_THREAD action', ()=>{
+    expect(a.viewThread(1,1)).toEqual({
+      type: c.VIEW_THREAD,
+      topicId: 1,
+      threadId: 1
+    });
+  });
 });
--- src/actions/index.js
+++ src/actions/index.js
@@ -7,11 +7,63 @@ export const addTopic = (newTopic) => {
     name,
     id
   });
-}
+};
 
-export const deleteTopic = (id) => {
-  return ({
+export const deleteTopic = (id) => ({
     type: c.DELETE_TOPIC,
+    id
+  }
+);
+
+export const addThread = (newThread) => {
+  const {name, topicId, id} = newThread;
+  return ({
+    type: c.ADD_THREAD,
+    name,
+    topicId,
     id
   });
 }
+
+export const deleteThread = (id) => ({
+    type: c.DELETE_THREAD,
+    id
+  }
+);
+
+export const addPost = (newPost) => {
+  const { title, body, date, score, id, threadId, topicId } = newPost;
+  return ({
+    type: c.ADD_POST,
+    title,
+    body,
+    date,
+    score,
+    id,
+    threadId,
+    topicId
+  });
+}
+
+export const deletePost = (id) => ({
+  type: c.DELETE_POST,
+  id
+});
+
+export const viewIndex = () => ({
+  type: c.VIEW_INDEX,
+  topicId: null,
+  threadId: null
+});
+
+export const viewTopic = (topicId) => ({
+  type: c.VIEW_TOPIC,
+  topicId,
+  threadId: null
+});
+
+export const viewThread = (topicId, threadId) => ({
+  type: c.VIEW_THREAD,
+  topicId,
+  threadId
+});
